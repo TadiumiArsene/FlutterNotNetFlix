6854ae6d66d3c536336cb791fcafaf4bedbf7c65
TadiumiArsene <arsenetadiumi@gmail.com>
2022-06-03 16:39:40 +0200

    starting project

---
+++ android/app/src/main/java/com/tadiumi/not_net_flix/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tadiumi.not_net_flix;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utils/constante.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color kBackgroundColor = const Color(0xfff020202);
+Color kPrimaryColor = const Color(0xfffe50914);

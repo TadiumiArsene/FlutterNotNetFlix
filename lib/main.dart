b063568972cb5663fd704240cadddf32183a7876
TadiumiArsene <arsenetadiumi@gmail.com>
2022-06-19 13:44:35 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NotNetFlix',
+      title: 'TadFlix',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
